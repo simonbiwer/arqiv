fbc881f50e038a1084f16ef0a5fabb945dddfddd
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2025-02-23 20:08:27 +0100

    improve db connection function

--- src/utils.ts
+++ src/utils.ts
@@ -6,14 +6,15 @@ export function createUUID(): UUID {
     return uuidv4() as UUID;
 }
 
-const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/arqivDB";
+const DB_NAME = "arqivDB"
+const MONGO_URI = process.env.MONGO_URI ?? `mongodb://localhost:27017/${DB_NAME}`;
 
 export async function connectDB() {
     try {
         await mongoose.connect(MONGO_URI);
-        console.log("MongoDB Connected Successfully!");
+        console.log("MongoDB connected successfully!");
     } catch (error) {
-        console.error("MongoDB Connection Error:", error);
+        console.error("MongoDB connection error:", error);
         process.exit(1);
     }
 }
